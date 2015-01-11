e3de74363f381f64facc1d8e69eb6f3f3c63947a
Jett <whackashoe@gmail.com>
2015-01-11 17:17:02 -0600

    make script good

--- scripts/killr.sh
+++ scripts/killr.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+stdin="$(ls -l /proc/self/fd/0)"
+stdin="${stdin/*-> /}"
 
-curl -X POST --data-binary @$1 http://killr.io
+if [[ "$stdin" =~ ^/dev/pts/[0-9] ]]; then
+    if [ $# -eq 0 ]
+        then
+            echo "No filename supplied"
+    else
+        curl -X POST --data-binary @$1 http://killr.io
+    fi
+else
+    curl -X POST --data-binary  @- http://killr.io
+fi
